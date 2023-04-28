97bb9b87a42548a42f40a0884233f8b19eeac1c4
Carla Sabatino <carlinhasabatino@gmail.com>
2023-04-28 20:17:07 -0300

    Primeiro commit

---
+++ src/componente/Banner/banner.js
@@ -0,0 +1,14 @@
+
+import './banner.css'
+
+function Banner(){
+    return (
+        <header className='banner'>
+<img src="/image/banner.png" alt=" Banner principal"/>
+
+        </header>
+
+    )
+}
+
+export default Banner
